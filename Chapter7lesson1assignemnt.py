30f69786fe6e143236a54196edf5256c27c31f02
oleg_andriushchenko <olegandriushchenko95@gmail.com>
2022-10-05 08:25:09 -0500

    Correcting homework for chapter 7 lesson 1, 10/05/2022

--- Chapter7lesson1assignemnt.py
+++ Chapter7lesson1assignemnt.py
@@ -1,15 +1,26 @@
 import random
 
-random.randrange(0, 50, 25)
+random_number = random.randrange(0, 50)
+
+attemts = 7
 
 number_to_guess = random
-user_input = input("Guess the number from 0 to 50\n")
+
+while attemts > 0:
+    user_input = input("Guess the number from 0 to 50\n")
+    user_input = int(user_input)
+
     if user_input > number_to_guess:
         print("The value is too low")
+        attemts -= 1
     elif user_input < number_to_guess:
         print("The value is too high")
+        attemts -= 1
     elif user_input == number_to_guess:
         print("You guessed correctly!")
+
+if attemts == 0:
+    print("No more attemts")
 
 
 
